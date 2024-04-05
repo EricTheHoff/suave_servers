49c06554ca7adc113d3582c692728f03dbcbb63d
Eric Hoffman <eric.peter.hoffman@gmail.com>
2024-04-05 00:18:01 -0600

    Removed unused imports

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 import PropTypes from 'prop-types';
 import './header.css';
 import man from '../../assets/man.png';
--- src/components/statusBtns/StatusBtns.jsx
+++ src/components/statusBtns/StatusBtns.jsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { statusBtns } from '../index.js';
